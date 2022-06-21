0a326bd96a56e236e509626e07db6149c16f451a
Vladislav <vlad.satonin74@gmail.com>
2022-06-21 15:17:41 +0500

    feat(commentsPage): add comments reducer & hooks to selector state

--- src/hooks/useCommentSlice.ts
+++ src/hooks/useCommentSlice.ts
@@ -1,9 +1,9 @@
-import {useAppSelector} from "./useReduxHooks";
+import { useAppSelector } from './useReduxHooks';
 
 export const UseCommentSlice = () => {
-	const comments = useAppSelector(state => state.comments);
+	const comments = useAppSelector((state) => state.comments);
 
 	return {
-		...comments
-	}
-}
+		...comments,
+	};
+};
